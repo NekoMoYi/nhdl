4d2fe8a056ff513f416ccc66e021a30f47bfb910
nekomoyi <funny_mo_yi@foxmail.com>
2024-01-22 23:17:39 +0800

    add support for cbz file and metadata

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
 import requests
-import bs4 as bs
 import json
-import re
 import flask_cors
 import threading
+import zipfile
+from xmlschema import XMLSchema, etree_tostring
 from tqdm import tqdm
 from flask import Flask, request, jsonify
 
 SAVE_DIR = "data/"
 MAX_RETRIES = 5
+
+PACK_AS_CBZ = True
+CBZ_DIR = "cbz/"
+XSD_FILE = "ComicInfo.xsd"
 
 
 def download(url, save_path=""):
@@ -19,6 +23,7 @@ def download(url, save_path=""):
         for data in response.iter_content(1024):
             f.write(data)
 
+
 def preview2download(url):
     filename = url.split("/")[-1]
     domain = url.split("/")[2]
@@ -26,6 +31,45 @@ def preview2download(url):
     url = url.replace(subdomain, subdomain.replace("t", "i"))
     url = url.replace(filename, filename.replace("t.", "."))
     return url
+
+
+def compressFolder(folder_path, zip_path):
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, arcname=os.path.relpath(
+                    file_path, folder_path))
+    zipf.close()
+
+
+def packAsCbz(gallery_id):
+    gallery_id = str(gallery_id)
+    with open(f"{SAVE_DIR}/{gallery_id}/info.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    schema = XMLSchema(XSD_FILE)
+    comicInfo = {
+        "Title": data["title"]["pretty"],
+        "Summary": "",
+        "Writer": ",".join(data["artists"]),
+        "Genre": ",".join(data["categories"]),
+        "Tags": "",
+        "Characters": ",".join(data["characters"]),
+        "SeriesGroup": ",".join(data["groups"])
+    }
+    if data["subtitle"]["pretty"] != "":
+        comicInfo["Title"] = data["subtitle"]["pretty"]
+    comicInfo["Summary"] = f"{data['title']['before']}{data['title']['pretty']}{data['title']['after']}\n{data['subtitle']['before']}{data['subtitle']['pretty']}{data['subtitle']['after']}"
+    tags = []
+    tags.extend(data["parodies"])
+    tags.extend(data["characters"])
+    tags.extend(data["tags"])
+    comicInfo["Tags"] = ",".join(tags)
+    root = schema.to_etree(comicInfo)
+    xmlFile = f"{SAVE_DIR}/{gallery_id}/ComicInfo.xml"
+    with open(xmlFile, "wb") as f:
+        f.write(etree_tostring(root, xml_declaration=True, encoding="utf-8"))
+    compressFolder(f"{SAVE_DIR}/{gallery_id}", f"{CBZ_DIR}/{gallery_id}.cbz")
 
 
 def downloadProcess(data):
@@ -43,6 +87,9 @@ def downloadProcess(data):
                 break
             except:
                 continue
+    if PACK_AS_CBZ:
+        packAsCbz(gallery_id)
+
 
 app = Flask(__name__)
 flask_cors.CORS(app)
@@ -58,4 +105,6 @@ def download_doujinshi():
 if __name__ == "__main__":
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
+    if not os.path.exists(CBZ_DIR) and PACK_AS_CBZ:
+        os.makedirs(CBZ_DIR)
     app.run(port=8901, debug=False)
