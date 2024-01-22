9ad656d5425d9e496bcdc3c0d38531c4eb79d7d6
nekomoyi <funny_mo_yi@foxmail.com>
2024-01-22 16:04:26 +0800

    add multithreading download support

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import bs4 as bs
 import json
 import re
 import flask_cors
+import threading
 from tqdm import tqdm
 from flask import Flask, request, jsonify
 
@@ -14,14 +15,9 @@ MAX_RETRIES = 5
 def download(url, save_path=""):
     filename = url.split("/")[-1]
     response = requests.get(url, stream=True)
-    file_size = int(response.headers.get("Content-Length", 0))
-    progress = tqdm(response.iter_content(
-        1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
     with open(os.path.join(save_path, filename), "wb") as f:
-        for data in progress:
+        for data in response.iter_content(1024):
             f.write(data)
-            progress.update(len(data))
-
 
 def preview2download(url):
     filename = url.split("/")[-1]
@@ -32,13 +28,7 @@ def preview2download(url):
     return url
 
 
-app = Flask(__name__)
-flask_cors.CORS(app)
-
-
-@app.route("/", methods=["POST"])
-def download_doujinshi():
-    data = request.get_json()
+def downloadProcess(data):
     imgs = data['imgs']
     gallery_id = data['id']
     gallery_dir = SAVE_DIR + "/" + gallery_id
@@ -46,13 +36,22 @@ def download_doujinshi():
         os.makedirs(gallery_dir)
     with open(gallery_dir + "/info.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    for img in tqdm(imgs, "Downloading images"):
+    for img in tqdm(imgs, f"Downloading {gallery_id}"):
         for _ in range(MAX_RETRIES):
             try:
                 download(preview2download(img), gallery_dir)
                 break
             except:
                 continue
+
+app = Flask(__name__)
+flask_cors.CORS(app)
+
+
+@app.route("/", methods=["POST"])
+def download_doujinshi():
+    data = request.get_json()
+    threading.Thread(target=downloadProcess, args=(data,)).start()
     return jsonify({"status": "ok"})
 
 
